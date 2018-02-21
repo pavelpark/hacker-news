6fb6731eb8d8e41e28bca912d197921da05ba374
Pavel Parkhomey <pavelpark@aol.com>
2018-02-21 14:29:24 -0800

    Readded HackerNewsVC.swift.

--- hacker-news/hacker-news/Controller/HackerNewsVC.swift
+++ hacker-news/hacker-news/Controller/HackerNewsVC.swift
@@ -2,37 +2,34 @@
 //  HackerNewsVC.swift
 //  hacker-news
 //
-//  Created by Pavel Parkhomey on 2/15/18.
+//  Created by Pavel Parkhomey on 2/21/18.
 //  Copyright © 2018 Pavel Parkhomey. All rights reserved.
 //
 
 import UIKit
 
 class HackerNewsVC: UIViewController {
-//    UITableViewDelegate, UITableViewDataSource
-    
 
-    // Outlets
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var urlLabel: UILabel!
-    @IBOutlet weak var pointsLabel: UILabel!
-    @IBOutlet weak var usernameLabel: UILabel!
-    @IBOutlet weak var timeLabel: UILabel!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
     }
 
-//    func numberOfSections(in tableView: UITableView) -> Int {
-//
-//    }
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//
-//    }
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//
-//    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
