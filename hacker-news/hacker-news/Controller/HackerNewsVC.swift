c64979de47db1d0def9868bd27b896a2af516fcf
Pavel Parkhomey <pavelpark@aol.com>
2018-02-21 14:48:13 -0800

    Added UIView to the HackerNewsVC and connected an outlet for menuBtn with SWRevealViewController to allow it to slide from left side.

--- hacker-news/hacker-news/Controller/HackerNewsVC.swift
+++ hacker-news/hacker-news/Controller/HackerNewsVC.swift
@@ -9,27 +9,16 @@
 import UIKit
 
 class HackerNewsVC: UIViewController {
-
+    
+    //Outlets
+    @IBOutlet weak var menuBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
